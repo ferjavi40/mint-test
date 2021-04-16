8dc38db2b62d2ee970a62f9836d7502dd6c8ce73
ferjavi40 <ferjaviramirez@gmail.com>
2021-04-16 20:46:32 +0200

    Fix: fix navbar name

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
