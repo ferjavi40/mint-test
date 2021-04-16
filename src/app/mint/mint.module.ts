d92dcaa5c925e06a9d20f87dad1b9db78ae402e0
ferjavi40 <ferjaviramirez@gmail.com>
2021-04-16 21:28:21 +0200

    feature: add carousel section

--- src/app/mint/mint.module.ts
+++ src/app/mint/mint.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselComponent } from './pages/carousel/carousel.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarouselComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CarouselComponent
   ]
 })
 export class MintModule { }
