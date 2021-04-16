53192bc32b05b24f227231aca0cd13b653e98832
ferjavi40 <ferjaviramirez@gmail.com>
2021-04-16 20:27:42 +0200

    Feature: declare all modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { MintModule } from './mint/mint.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    MintModule,
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
