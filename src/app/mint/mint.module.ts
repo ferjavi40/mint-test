0e1ca570cc8bc73a79c88dfb53906ac9437fcdc3
ferjavi40 <ferjaviramirez@gmail.com>
2021-04-17 15:37:18 +0200

    feature: add about

--- src/app/mint/mint.module.ts
+++ src/app/mint/mint.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from './pages/carousel/carousel.component';
 import { SharedModule } from '../shared/shared.module';
+import { AboutComponent } from './pages/about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    CarouselComponent
+    CarouselComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    CarouselComponent
+    CarouselComponent,
+    AboutComponent
   ]
 })
 export class MintModule { }
